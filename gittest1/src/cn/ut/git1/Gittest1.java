47e9ff681ff0a2b13b34c7b8806f814f6b6ccc53
LYY <LYY@LYY-PC>
2017-12-04 22:52:17 +0800

    第二次测试

--- gittest1/src/cn/ut/git1/Gittest1.java
+++ gittest1/src/cn/ut/git1/Gittest1.java
@@ -2,6 +2,8 @@ package cn.ut.git1;
 
 public class Gittest1 {
 	public static void main(String[] args) {
-		System.out.println("get测试1");
+		System.out.println("get测试2");
+		
+		System.out.println("get测试22222");
 	}
 }
